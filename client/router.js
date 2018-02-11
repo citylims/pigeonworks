f8e13ad5c9abbbd9acdd4235c37d5bc3b05d1160
Ausin Formica <austin.formica@gmail.com>
2018-02-11 14:27:13 -0500

    remove canvas on route change

--- client/router.js
+++ client/router.js
@@ -1,11 +1,26 @@
 FlowRouter.route('/', {
   action: function(params, queryParams) {
     BlazeLayout.render('dashLayout', { scene: "creationMyth"});
+  },
+  triggersExit: function() {
+    $('canvas').remove(); // this should be moved to router level.
   }
 });
 
 FlowRouter.route('/flight', {
   action: function(params, queryParams) {
     BlazeLayout.render('dashLayout', { scene: "flight"});
+  },
+  triggersExit: function() {
+      $('canvas').remove(); // this should be moved to router level.
+  }
+});
+
+FlowRouter.route('/pygmalion', {
+  action: function(params, queryParams) {
+    BlazeLayout.render('dashLayout', { scene: "pygmalion"});
+  },
+  triggersExit: function() {
+      $('canvas').remove(); // this should be moved to router level.
   }
 });
