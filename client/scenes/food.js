3aa893e1e8cd406ca23b12958f73b8119109bbef
Ausin Formica <austin.formica@gmail.com>
2018-02-25 13:25:05 -0500

    more food stuffs

--- client/scenes/food.js
+++ client/scenes/food.js
@@ -1,4 +1,14 @@
 Template.food.onCreated(function() {
+  this.foodCount = new ReactiveVar(0);
+  this.foodMax = new ReactiveVar(31);
+  this.gravX = new ReactiveVar(0);
+  this.gravY = new ReactiveVar(0);
+  this.impulseForce = new ReactiveVar(180);
+  this.impulseX = new ReactiveVar(0);
+  this.impulseY = new ReactiveVar(2);
+  this.damping = new ReactiveVar(0);
+  this.bounce = new ReactiveVar(0.4);
+  
   this.foodTypes = [
     {
       name: "donut",
@@ -30,15 +40,15 @@ Template.food.onCreated(function() {
     },
   ];
   
-  var i = 0
+
   this.addFood = (key) => {
-    //ugly hardcode to avoid limit bug
-    if (i === 31) {
+    if (this.foodCount.get() === this.foodMax.get()) {
       console.log("NOPE");
-      console.log($('.grav'))
-      return
-    } 
-    i++
+      console.log($('.grav'));
+      return;
+    }
+     
+    this.foodCount.set(this.foodCount.get() + 1);
     
     var el = document.createElement('span');
     var item = this.foodTypes[Math.floor(Math.random() * this.foodTypes.length)];
@@ -47,27 +57,37 @@ Template.food.onCreated(function() {
     $(el).css('position', "absolute");
     
     var width = window.innerWidth;
-    var rando = Math.floor(Math.random() * width) + 1;
-    $(el).css('left', `${rando}px`);
-    $('body').prepend(el) //dont matter cuz plugin moves to body level no matter what
-    
-    var gr_x = 0;
-  	var gr_y = 0; //zero grav
+    var posX = Math.floor(Math.random() * width) + 1;
+    $(el).css('left', `${posX}px`);
+    $('body').prepend(el) //dont matter cuz plugin moves to body level no matter what 
+    var rangeImpulseX = Math.floor(Math.random() * 4) + 1;
+    var check = Math.floor(Math.random() * 2);
+    if (!check) {
+      rangeImpulseX = -Math.abs(Math.floor(Math.random() * 4) + 1);
+    }
+    var rangeImpulseY = Math.floor(Math.random() * 3) + 1;
     
     $(el).throwable({
       containment:[0,0,window.innerWidth,(window.innerHeight - 30)],
       drag: true,
-      gravity: {x: gr_x, y: gr_y},
+      gravity: {x: this.gravX.get(), y: this.gravY.get()},
       impulse: {
-        f: 300,
-        p: {x: 0, y: 2}
+        f: this.impulseForce.get(),
+        p: {x: rangeImpulseX, y: rangeImpulseY}
       },
       shape: "circle",
       autostart: true,
-      bounce: 0.2,
-      damping: 0
+      bounce: this.bounce.get(),
+      damping: this.damping.get()
     });
-  }
+  };
+  
+  this.autoAdd = () => {
+    var adding = Meteor.setInterval(() => {
+      this.addFood();
+    }, 5000);
+  };
+  
 });
 
 Template.food.helpers({
@@ -83,4 +103,6 @@ Template.food.events({
 });
 
 Template.food.onRendered(function() {
+  var inst = Template.instance();
+  inst.autoAdd();
 });
