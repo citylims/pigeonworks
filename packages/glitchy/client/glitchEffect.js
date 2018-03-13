9f822be9c7b8384838feceeaea74d858f505c0da
Ausin Formica <austin.formica@gmail.com>
2018-03-13 15:19:20 -0400

    working glitchy

--- packages/glitchy/client/glitchEffect.js
+++ packages/glitchy/client/glitchEffect.js
@@ -1,104 +1,46 @@
 import THREE from 'three';
 import { Promise } from 'meteor/promise';
-// import glslify from 'glslify';
 
-export default class PostEffect {
-  exp(uniforms) {
-    return new Promise((resolve, reject) => {
-      var obj = this.createObj(uniforms);
-      obj.then((res) => {
-        if (!res) reject(res);
-        console.log(res);
-        resolve(res);
-      });
-    });
-  }
-  constructor(texture) {
-    return new Promise((resolve, reject) => {
-      this.uniforms = {
-        time: {
-          type: 'f',
-          value: 0
-        },
-        resolution: {
-          type: 'v2',
-          value: new THREE.Vector2(document.body.clientWidth, window.innerHeight)
-        },
-        texture: {
-          type: 't',
-          value: texture,
-        },
-      };
-      var foo = this.exp(this.uniforms);
-      foo.then((res) => {
-        if (!res) reject(res);
-        this.obj = res;
-        
-        // console.log(res);
-        this.render = (time) => {
-          if (this.uniforms) {
-            this.uniforms.time.value += time;
-          }
-        }
-        this.resize = () => {
-          if (this.uniforms) {
-            this.uniforms.resolution.value.set(document.body.clientWidth, window.innerHeight);
-          }
-        }
-        resolve(this);
-      });
-    });
-  }
-  createObj(uniforms) {
-    return new Promise((resolve, reject) => { 
-      
-      var foo = Meteor.call('readGlsl', 'glitchEffect.vs');
-      console.log(foo);
-      
-      Meteor.call('readGlsl', 'glitchEffect.vs', (effectShader, err) => {
-        console.log(effectShader)
-        console.log(err)
-        if (err) reject(err);
-        // console.log(res);
-        Meteor.call('readGlsl', 'glitchEffect.fs', (effectFrag, err) => {
-          console.log(err);
-          if (err) reject(err);
-          console.log('runnin')
-          console.log(effectFrag);
-          var mesh = new THREE.Mesh(
-            new THREE.PlaneBufferGeometry(2, 2),
-            new THREE.RawShaderMaterial({
-              uniforms: uniforms,
-              // vertexShader: glslify('../glsl/glitchEffect.vs'),
-              vertexShader: effectShader,
-              // fragmentShader: glslify('../glsl/glitchEffect.fs'),
-              fragmentShader: effectFrag,
-            })
-          );
-          resolve(mesh);
-        });
-      });
-    })
+export const createEffect = function(texture) {
+  return new Promise(function(resolve, reject) {
+    let effect = {};
+    effect.uniforms = {
+      time: {
+        type: 'f',
+        value: 0
+      },
+      resolution: {
+        type: 'v2',
+        value: new THREE.Vector2(document.body.clientWidth, window.innerHeight)
+      },
+      texture: {
+        type: 't',
+        value: texture,
+      },
+    };
+    effect.render = (time) => {
+      if (effect.uniforms) {
+        effect.uniforms.time.value += time;
+      }
+    }
+    effect.resize = () => {
+      if (effect.uniforms) {
+        effect.uniforms.resolution.value.set(document.body.clientWidth, window.innerHeight);
+      }
+    }
     
-    // return new THREE.Mesh(
-    //   new THREE.PlaneBufferGeometry(2, 2),
-    //   new THREE.RawShaderMaterial({
-    //     uniforms: this.uniforms,
-    //     // vertexShader: glslify('../glsl/glitchEffect.vs'),
-    //     vertexShader: Shaders.effectShader,
-    //     // fragmentShader: glslify('../glsl/glitchEffect.fs'),
-    //     fragmentShader: Shaders.effectFragShader,
-    //   })
-    // );
-  }
-  render(time) {
-    if (this.uniforms) {
-      this.uniforms.time.value += time;
-    }
-  }
-  resize() {
-    if (this.uniforms) {
-      this.uniforms.resolution.value.set(document.body.clientWidth, window.innerHeight);
-    }
-  }
+    Meteor.call('fetchGlsl', 'glitchEffect.vs', (errV, effectShader) => {
+      Meteor.call('fetchGlsl', 'glitchEffect.fs', (errF, effectFrag) => {
+        effect.mesh = new THREE.Mesh(
+          new THREE.PlaneBufferGeometry(2, 2),
+          new THREE.RawShaderMaterial({
+            uniforms: effect.uniforms,
+            vertexShader: effectShader,
+            fragmentShader: effectFrag,
+          })
+        );
+        resolve(effect)
+      });
+    });
+  });
 }
--- packages/glitchy/client/glitchy.js
+++ packages/glitchy/client/glitchy.js
@@ -1,148 +1,79 @@
 import THREE from 'three';
+import { Promise } from 'meteor/promise';
 import GlitchImage from './glitchImage.js';
-// import PostEffect from '../modules/sketch/glitch/PostEffect.js';
 import GlitchEffect from './glitchEffect.js';
 
 
 Template.glitchy.onCreated(function() {
-  // this.loaded = false
-  
+  this.loadedImage = new ReactiveVar(false)
+  this.loadedEffect = new ReactiveVar(false)
 });
 
 Template.glitchy.onRendered(function() {
-    var baseUrl = window.location.href;
-    console.log(baseUrl)
-    const canvas = document.getElementById('canvas-webgl');
-    const renderer = new THREE.WebGLRenderer({
-      antialias: false,
-      canvas: canvas,
-    });
-    const renderBack1 = new THREE.WebGLRenderTarget(document.body.clientWidth, window.innerHeight);
-    const scene = new THREE.Scene();
-    const sceneBack = new THREE.Scene();
-    const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
-    const cameraBack = new THREE.PerspectiveCamera(45, document.body.clientWidth / window.innerHeight, 1, 10000);
-    const clock = new THREE.Clock();
-
-    const vectorTouchStart = new THREE.Vector2();
-    const vectorTouchMove = new THREE.Vector2();
-    const vectorTouchEnd = new THREE.Vector2();
-
-    let isDrag = false;
-
-    //
-    // process for this sketch.
-    //
-    var bgImg;
-    var glitchEffect;
-    var foo = new GlitchImage();
-    foo.then((res) => {
-      console.log(res);
-      bgImg = res;
-      console.log(bgImg);
-      var bar = new GlitchEffect(renderBack1.texture);
-      bar.then((res) => {
-        console.log(res);
-        glitchEffect = res;
-          const resizeWindow = () => {
-            canvas.width = document.body.clientWidth;
-            canvas.height = window.innerHeight;
-            cameraBack.aspect = document.body.clientWidth / window.innerHeight;
-            cameraBack.updateProjectionMatrix();
-            bgImg.resize();
-            glitchEffect.resize();
-            renderBack1.setSize(document.body.clientWidth, window.innerHeight);
-            renderer.setSize(document.body.clientWidth, window.innerHeight);
-          }
-          const render = () => {
-            const time = clock.getDelta();
-            renderer.render(sceneBack, cameraBack, renderBack1);
-            glitchEffect.render(time);
-            renderer.render(scene, camera);
-          }
-          const renderLoop = () => {
-            render();
-            requestAnimationFrame(renderLoop);
-          }
-          const touchStart = (isTouched) => {
-            isDrag = true;
-          };
-          const touchMove = (isTouched) => {
-            if (isDrag) {}
-          };
-          const touchEnd = (isTouched) => {
-            isDrag = false;
-          };
-          const on = () => {
-            window.addEventListener('resize', GlitchyApi.debounce(() => {
-              resizeWindow();
-            }), 1000);
-            canvas.addEventListener('mousedown', function (event) {
-              event.preventDefault();
-              vectorTouchStart.set(event.clientX, event.clientY);
-              GlitchyApi.normalizeVector2(vectorTouchStart);
-              touchStart(false);
-            });
-            document.addEventListener('mousemove', function (event) {
-              event.preventDefault();
-              vectorTouchMove.set(event.clientX, event.clientY);
-              GlitchyApi.normalizeVector2(vectorTouchMove);
-              touchMove(false);
-            });
-            document.addEventListener('mouseup', function (event) {
-              event.preventDefault();
-              vectorTouchEnd.set(event.clientX, event.clientY);
-              GlitchyApi.normalizeVector2(vectorTouchEnd);
-              touchEnd(false);
-            });
-            canvas.addEventListener('touchstart', function (event) {
-              event.preventDefault();
-              vectorTouchStart.set(event.touches[0].clientX, event.touches[0].clientY);
-              GlitchyApi.normalizeVector2(vectorTouchStart);
-              touchStart(event.touches[0].clientX, event.touches[0].clientY, true);
-            });
-            canvas.addEventListener('touchmove', function (event) {
-              event.preventDefault();
-              vectorTouchMove.set(event.touches[0].clientX, event.touches[0].clientY);
-              GlitchyApi.normalizeVector2(vectorTouchMove);
-              touchMove(true);
-            });
-            canvas.addEventListener('touchend', function (event) {
-              event.preventDefault();
-              vectorTouchEnd.set(event.changedTouches[0].clientX, event.changedTouches[0].clientY);
-              GlitchyApi.normalizeVector2(vectorTouchEnd);
-              touchEnd(true);
-            });
-          }
-
-          this.init = () => {
-            renderer.setSize(document.body.clientWidth, window.innerHeight);
-            renderer.setClearColor(0x555555, 1.0);
-            cameraBack.position.set(1000, 1000, 1000);
-            cameraBack.lookAt(new THREE.Vector3());
-            
-            bgImg.init(() => {
-              sceneBack.add(bgImg.obj);
-              scene.add(glitchEffect.obj);
-            })
-
-            on();
-            resizeWindow();
-            renderLoop();
-          }
-        this.init();
-      });
-    });
-    // const bgImg = new GlitchImage();
-    
-    // const glitchEffect = new GlitchEffect(renderBack1.texture);
-    
-
-    //
-    // common process
-    //
+  var inst = Template.instance();
+  const canvas = document.getElementById('glitchyCanvas');
+  const renderer = new THREE.WebGLRenderer({
+    antialias: false,
+    canvas: canvas,
+  });
+  const renderBack1 = new THREE.WebGLRenderTarget(document.body.clientWidth, window.innerHeight);
+  const scene = new THREE.Scene();
+  const sceneBack = new THREE.Scene();
+  const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+  const cameraBack = new THREE.PerspectiveCamera(45, document.body.clientWidth / window.innerHeight, 1, 10000);
+  const clock = new THREE.Clock();
+  const resizeWindow = () => {
+    canvas.width = document.body.clientWidth;
+    canvas.height = window.innerHeight;
+    cameraBack.aspect = document.body.clientWidth / window.innerHeight;
+    cameraBack.updateProjectionMatrix();
+    // img.resize();
+    // effect.resize();
+    renderBack1.setSize(document.body.clientWidth, window.innerHeight);
+    renderer.setSize(document.body.clientWidth, window.innerHeight);
+  }
+  const render = () => {
+    const time = clock.getDelta();
+    renderer.render(sceneBack, cameraBack, renderBack1);
+    var effect = inst.loadedEffect.get();
+    if (effect) {
+      effect.render(time);
+    }
+    renderer.render(scene, camera);
+  }
+  const renderLoop = () => {
+    render();
+    requestAnimationFrame(renderLoop);
+  }
+  // 
+  // const vectorTouchStart = new THREE.Vector2();
+  // const vectorTouchMove = new THREE.Vector2();
+  // const vectorTouchEnd = new THREE.Vector2();
+  // let isDrag = false;
   
-  // }
+  inst.autorun(() => {
+    if (this.loadedImage.get() && this.loadedEffect.get()) {
+      renderer.setSize(document.body.clientWidth, window.innerHeight);
+      renderer.setClearColor(0x555555, 1.0);
+      cameraBack.position.set(1000, 1000, 1000);
+      cameraBack.lookAt(new THREE.Vector3());
+      var img = this.loadedImage.get()
+      var effect = this.loadedEffect.get()
+      sceneBack.add(img.mesh);
+      scene.add(effect.mesh);
+      // on();
+      renderLoop();
+    }
+  });
+  
+  GlitchImage.createImage().then(function(res) {
+    inst.loadedImage.set(res);
+  });
+
+  GlitchEffect.createEffect(renderBack1.texture).then(function(res) {
+    inst.loadedEffect.set(res);
+  });
+  
 });
 
 
