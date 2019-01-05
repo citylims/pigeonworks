35b52a7bd1f23d57e502b8727a1c8a79f7615656
Ausin Formica <austin.formica@gmail.com>
2019-01-05 15:06:37 -0500

    layout set root

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+BlazeLayout.setRoot('body');
 Template.registerHelper('windowHeight', function(date) {
   return `${window.innerHeight}`;
 });
