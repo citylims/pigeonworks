fed7f1792ebb93cb2bb45c159b42fa0c7d2530a0
Ausin Formica <austin.formica@gmail.com>
2019-07-05 13:50:19 -0700

    basic layout for violins, violence, silence

---
+++ client/scenes/silence.js
@@ -0,0 +1,13 @@
+Template.silence.onCreated(function() {
+// https://threejs.org/examples/#webgl_lines_colors
+// http://kadekeith.me/stuff/three/glow/
+// https://codepen.io/robin-dela/pen/WjGoLJ  
+});
+
+Template.silence.helpers({
+});
+
+Template.silence.events({
+});
+
+
